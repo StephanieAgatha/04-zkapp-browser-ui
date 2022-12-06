d85eb10e7645818f1380ef2ee5e9e91b94d1f66a
Stephanie Agatha <ellizavad@gmail.com>
2022-12-07 00:54:49 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-f566b640ebadb3c4.js
+++ _next/static/chunks/webpack-f566b640ebadb3c4.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "b8db27d1b310ba95" + ".css";
+/******/ 			return "static/css/" + "a6ab410a6c85c842" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
